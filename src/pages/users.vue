775e9a97bc6cedbff5aadd172abc8fe6318fac3d
Masutayunikon <quentinmail974@gmail.com>
2023-06-21 02:26:12 +0400

    🐛 fix create user

--- src/pages/users.vue
+++ src/pages/users.vue
@@ -7,7 +7,7 @@
             <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
           </div>
           <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <button type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <button @click="router.push('/register')" type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               <PlusIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
               New user
             </button>
